257faf9cfbf32d0e7e3d959cbc8eaec3c6932059
rabenjasolo <rabenjasolo@gmail.com>
2021-05-24 18:30:34 +0300

    Refacto: Isolating the Application (Domain) Logic

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 
-$connection = new PDO("mysql:host=localhost;dbname=blogpoo", 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+require_once('model.php');
 
-$result = $connection->query('SELECT id, title FROM articles');
-
-$posts = [];
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    $posts[] = $row;
-}
-
-$connection = null;
+$posts = get_all_posts();
 
 // include the HTML presentation code
 require 'templates/list.php';
