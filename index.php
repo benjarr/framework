d4907ae0d432e42fc6d59d5cff63c32dea2e2274
rabenjasolo <rabenjasolo@gmail.com>
2021-05-24 17:45:03 +0300

    Refacto: Isolating the Presentation

--- index.php
+++ index.php
@@ -3,28 +3,13 @@
 $connection = new PDO("mysql:host=localhost;dbname=blogpoo", 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 $result = $connection->query('SELECT id, title FROM articles');
-?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-            <li>
-                <a href="/show.php?id=<?= $row['id'] ?>">
-                    <?= $row['title'] ?>
-                </a>
-            </li>
-            <?php endwhile ?>
-        </ul>
-    </body>
-</html>
+$posts = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $posts[] = $row;
+}
 
-<?php
 $connection = null;
-?>
+
+// include the HTML presentation code
+require 'templates/list.php';
