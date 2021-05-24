1b6eff9b9ff8ede1b7bd9078510be5fb710ccc6e
rabenjasolo <rabenjasolo@gmail.com>
2021-05-24 18:57:12 +0300

    Refacto: Isolating the Layout

--- templates/list.php
+++ templates/list.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php foreach ($posts as $post): ?>
-            <li>
-                <a href="/show.php?id=<?= $post['id'] ?>">
-                    <?= $post['title'] ?>
-                </a>
-            </li>
-            <?php endforeach ?>
-        </ul>
-    </body>
-</html>
+<?php $title = "List of Posts"; ?>
+
+<?php ob_start() ?>
+    <h1><?= $title ?></h1>
+    <ul>
+        <?php foreach ($posts as $post): ?>
+        <li>
+            <a href="/show.php?id=<?= $post['id'] ?>">
+                <?= $post['title'] ?>
+            </a>
+        </li>
+        <?php endforeach ?>
+    </ul>
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php'; ?>
