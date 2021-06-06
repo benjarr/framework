4b749c87c4c020a22b2631784fb5785bc1279e53
benjarr <rabenjasolo@gmail.com>
2021-06-06 18:22:19 +0300

    Symfony: Templating

--- src/app.php
+++ src/app.php
@@ -4,7 +4,16 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection;
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
+
+$routes->add('hello', new Route('/hello/{name}',[
+    'name'        => 'World',
+    '_controller' => function ($request) {
+        $response = render_tempate($request);
+
+        return $response;
+    }
+]));
+
 $routes->add('bye', new Route('/bye'));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,19 +8,25 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
+function render_tempate($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+    
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
+$routes = include __DIR__ . '/../src/app.php';
 
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('_controller', $request);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Page Not Found', 404);
 } catch (Exception $exception) {
