5d9839dbaabcdced43be6a36a4a8c18d4909feee
benjarr <rabenjasolo@gmail.com>
2021-06-06 21:07:53 +0300

    Symfony: HttpKernel: Controller Resolver

--- src/app.php
+++ src/app.php
@@ -5,13 +5,18 @@ use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection;
 
-$routes->add('hello', new Route('/hello/{name}',[
+$routes->add('hello', new Route('/hello/{name}', [
     'name'        => 'World',
     '_controller' => function ($request) {
         $response = render_tempate($request);
 
         return $response;
     }
+]));
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', [
+    'year'        => null,
+    '_controller' => 'LeapYearController::index',
 ]));
 
 $routes->add('bye', new Route('/bye'));
--- web/front.php
+++ web/front.php
@@ -4,16 +4,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
-function render_tempate($request)
+function render_tempate(Request $request)
 {
     extract($request->attributes->all(), EXTR_SKIP);
     ob_start();
     include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-    
+
     return new Response(ob_get_clean());
 }
 
@@ -24,9 +26,16 @@ $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func('_controller', $request);
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Page Not Found', 404);
 } catch (Exception $exception) {
@@ -34,3 +43,24 @@ try {
 }
 
 $response->send();
+
+function is_leap_year($year = null)
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+class LeapYearController
+{
+    public function index($year)
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
